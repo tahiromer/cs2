5c1efabae9cf3a890900c8a481bc91de7dc441d8
tahiromer <creationsofcloud@gmail.com>
2015-12-27 21:25:00 -0500

    Commit from Netbeans

---
+++ Period5Project1/src/period5project1/Card.java
@@ -0,0 +1,42 @@
+package period5project1;
+
+/**
+ *
+ * @author Tahir
+ */
+public class Card { 
+    String suit;
+    String value;
+    
+    public Card(){
+        this.suit = "JOKER";
+        this.value = "NO SUIT";
+    }
+    
+    public Card(String suit, String value){
+        this.suit = suit;
+        this.value = value;
+    }
+    
+    public static void main(String[] args){
+        Card c1 = new Card();
+        System.out.println(c1.suit);
+        System.out.println(c1.value);
+   
+        Card c2= new Card("clubs", "2");
+        System.out.println(c2.suit);
+        System.out.println(c2.value);
+        
+        Card c3= new Card("hearts", "K");
+        System.out.println(c3.suit);
+        System.out.println(c3.value);
+        
+        Card c4= new Card("spades", "A");
+        System.out.println(c4.suit);
+        System.out.println(c4.value);
+
+    
+    }
+}
+
+ 
