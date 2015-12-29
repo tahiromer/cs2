9a92606843aea5e09e2fbaffd38ed4ef311e94f9
tahiromer <creationsofcloud@gmail.com>
2015-12-29 14:19:56 -0500

    Commit from Netbeans

---
+++ Period5Project1/src/period5project1/CardApp.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package period5project1;
+import java.util.*;
+/**
+ *
+ * @author Tahir
+ */
+public class CardApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int[] deck = new int[52];
+    String[] suit = {"S", "D", "C", "H"};
+    String[] value = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    
+    for( int i = 0; i <deck.length; i++) deck[i] = 1;
+//    for( int i = ; i <deck.length; i++)
+//System.out.println(deck[i]);
+    
+        
+    }
+    
+}
+    
